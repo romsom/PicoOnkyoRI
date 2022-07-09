dc4cc84e797db07ff57fce2f9345cb61179be723
Roman Sommer <roman@resonant-bytes.de>
2022-07-09 21:19:40 +0200

    onkyo: sampling in PIO, decoding in Python

--- OnkyoRI/onkyo_ri.py
+++ OnkyoRI/onkyo_ri.py
@@ -18,23 +18,39 @@ import rp2
 def wait_pin_high():
     wrap_target()
 
-    set(x, 7) # read 8 words
-    set(y, 31) # with 32 bits each
+    set(x, 7) # read 8 header high bits
 
     wait(1, pin, 0)
     
-    label("read")
+    label("read_header")
     in_(pins, 1)
-    jmp(y_dec, "read_delayed")
-    set(y, 31) # with 32 bits each
-    jmp(x_dec, "read") # continue reading next word
+    jmp(x_dec, "read_header")
+    nop()[1] # 9th bit
+    # 3 header low bits
+    nop()[1] # 1st bit
+    in_(pins, 1)[1] # in_(pins, 1)
+    nop() # 3rd bit
 
-    jmp("irq")
+    set(x, 17) # 12 bits, one high phase, possibly 2 low -> 36 phases -> 36 bits
+    label("read_bit")
+    nop()[1] # 1st sample
+    in_(pins, 1)[1]
+    nop()[1] # 3rd sample
+    # next phase
+    nop()[1] # 1st sample
+    in_(pins, 1)[1]
+    nop() # 3rd sample
+    jmp(x_dec, "read_bit")
 
-    label("read_delayed")
-    jmp("read")
+    # we have 8 + 1 + 36 = 45 bits, so we need to pad them to 64 bits
+    set(x, 18) # 12 bits, one high phase, possibly 2 low -> 36 phases -> 36 bits
 
-    label("irq")
+    label("read_padding_bit")
+    in_(pins, 1)
+    jmp(x_dec, "read_padding_bit")
+
+    # push()
+
     irq(block, rel(0))
 
 
@@ -42,16 +58,72 @@ def wait_pin_high():
 def handler(sm):
     # Print a (wrapping) timestamp, and the state machine object.
     n_words = sm.rx_fifo()
-    res = ''
-    for _ in range(n_words):
-        res += f'{sm.get():032b}'
+
+    if n_words >= 2:
+        word_0 = sm.get()
+        if (word_0 >> 23) != 0x1fe:
+            print('invalid header')
+            return
+        # res = 0
+        # next_sample = 23
+        # bits_remaining = 12
+
+        # while next_sample >= 2:
+        #     hi_state = (word_0 >> next_sample) & 0x1
+        #     if hi_state != 1:
+        #         print('invalid data')
+        #         return
+
+        #     bit_state = (word_0 >> (next_sample - 2)) & 0x1
+        #     res <<= 1
+        #     res += bit_state
+        #     bits_remaining -= 1
+        #     next_sample -= 3 - bit_state # advance 2 or 3 bits
+
+        # if next_sample == 1:
+        #     hi_state = (word_0 >> next_sample) & 0x1
+        #     if hi_state != 1:
+        #         print('invalid data')
+        #         return
+
+        #     bit_state = (word_0 >> (next_sample - 2)) & 0x1
+        #     res <<= 1
+        #     res += bit_state
+        #     bits_remaining -= 1
+        #     next_sample -= 2 + bit_state # advance 2 or 3 bits
+        word_1 = sm.get()
+        data = ((word_0 & 0x7fffff) << 32 + 9) + (word_1 << 9)
+        n_remaining = n_words - 2
+        while n_remaining > 0:
+            print('found extra data: ', sm.get())
+            n_remaining -= 1
+    
+        res = 0
+        next_sample = 63
+        bits_remaining = 12
+
+        while bits_remaining > 0:
+            hi_state = (data >> next_sample) & 0x1
+            if hi_state != 1:
+                print('invalid data')
+                return
+
+            bit_state = (data >> (next_sample - 2)) & 0x1
+            res <<= 1
+            res += (1-bit_state)  # somehow making this inverse makes it match the lirc docs for tape control
+            bits_remaining -= 1
+            next_sample -= 3 - bit_state # advance 2 or 3 bits
+                
+            
         
-    print(time.ticks_ms(), res)
+        print(time.ticks_ms(), f'0x{res:03x}')
+        # print(f'{word_0:032b}')
+        # print(f'{word_1:032b}')
 
 
 # Instantiate StateMachine(0) with wait_pin_low program on Pin(16).
 input_pin= Pin(15, Pin.IN, Pin.PULL_UP)
-sm0 = rp2.StateMachine(0, wait_pin_high, freq=12000, in_base=input_pin)
+sm0 = rp2.StateMachine(0, wait_pin_high, freq=6000, in_base=input_pin)
 sm0.irq(handler)
 
 # Start the StateMachine's running.
